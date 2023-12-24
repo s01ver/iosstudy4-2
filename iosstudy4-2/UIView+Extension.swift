8544be8208f49e05d1a6081aeff7a9809ad48761
s01ver <solverglace@gmail.com>
2023-12-24 19:37:47 +0900

    Initial Commit

---
+++ iosstudy4-2/UIView+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Extension.swift
+//  iosstudy4-2
+//
+//
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
